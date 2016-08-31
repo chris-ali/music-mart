03e4bc5429217a3d76ee20f1adc2ef302a3d367c
Christopher Ali <christopher.ali@outlook.com>
2016-08-31 14:59:45 -0400

    DataSource and Security Congifuration
    - Added DataSourceProductionConfiguration and
    DataSourceTestConfiguration classes to separate test and production
    configurations
    
    - Configured DataSourceTestConfiguration to add transactionManager
    sessionFactory and hibernateExceptionTranslator beans
    
    - Added hibernate properties to application-test to enable hbm2ddl
    update mode

---
+++ src/main/java/com/chrisali/musicmart/config/DataSourceTestConfiguration.java
@@ -0,0 +1,73 @@
+package com.chrisali.musicmart.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@Profile("test")
+public class DataSourceTestConfiguration {
+
+	@Autowired
+	Environment env;
+
+	@Bean(name = "testDataDource")
+	public DataSource dataSource() {
+		DataSourceBuilder builder = DataSourceBuilder.create();
+
+		builder.driverClassName(env.getProperty("spring.datasource.driver-class-name"))
+				.url(env.getProperty("spring.datasource.url"))
+				.username(env.getProperty("spring.datasource.username"))
+				.password(env.getProperty("spring.datasource.password"));
+
+		return builder.build();
+	}
+
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource());
+		sessionFactory.setPackagesToScan(new String[] {"com.chrisali.musicmart.model",
+													   "com.chrisali.musicmart.dao",
+													   "com.chrisali.musicmart.service"});
+		sessionFactory.setHibernateProperties(hibernateProperties());
+
+		return sessionFactory;
+	}
+
+	Properties hibernateProperties() {
+		return new Properties() {
+			private static final long serialVersionUID = 9057467730041849300L;
+			{
+				setProperty("hibernate.dialect", env.getProperty("spring.jpa.properties.hibernate.dialect"));
+				setProperty("hibernate.hbm2ddl.auto", env.getProperty("spring.jpa.properties.hibernate.hbm2ddl.auto"));
+			}
+		};
+	}
+
+	@Bean
+	public DataSourceTransactionManager transactionManager(SessionFactory sessionFactory) {
+		DataSourceTransactionManager txManager = new DataSourceTransactionManager();
+		txManager.setDataSource(dataSource());
+
+		return txManager;
+	}
+
+	@Bean
+	public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+		return new PersistenceExceptionTranslationPostProcessor();
+	}
+}
